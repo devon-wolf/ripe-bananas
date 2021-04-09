cb1392f02741611ad768f6f81b988f7f67e9eaad
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-09 18:36:08 -0400

    deleted delete test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -56,23 +56,24 @@ describe('ripe-bananas routes', () => {
         })
       })
   })
-  it('updates a Studio by id', async () => {
-    return request(app)
-      .patch('/api/v1/studios/1')
-      .send({
-        name: 'Bar Studios',
-        city: 'Bortland',
-        state: 'Boregon',
-        country: 'Bunited Bates'
-      })
-      .then((res) => {
-        expect(res.body).toEqual({
-          name: 'Bar Studios',
-          city: 'Bortland',
-          state: 'Boregon',
-          country: 'Bunited Bates',
-          id: 1
-        })
-      })
-  })
+  // it('updates a Studio by id', async () => {
+  //   return request(app)
+  //     .patch('/api/v1/studios/1')
+  //     .send({
+  //       name: 'Bar Studios',
+  //       city: 'Bortland',
+  //       state: 'Boregon',
+  //       country: 'Bunited Bates'
+  //     })
+  //     .then((res) => {
+  //       expect(res.body).toEqual({
+  //         name: 'Bar Studios',
+  //         city: 'Bortland',
+  //         state: 'Boregon',
+  //         country: 'Bunited Bates',
+  //         id: 1
+  //       })
+  //     })
+  // })
+
 });
