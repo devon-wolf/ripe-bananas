54d18ef7b7d7ef19bc76e7c8b40f03d33cda161c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-09 17:42:31 -0700

    Add associations file and pare back review model

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -1,14 +1,15 @@
+require('../lib/models/associations');
 const db = require('../lib/utils/database');
 const request = require('supertest');
 const app = require('../lib/app');
 const Review = require('../lib/models/Review');
 
-const seedReview = {
-	rating: 2,
-	reviewer: 1,
-	review: 'This is almost the worst thing ever',
-	film: 1
-}
+// const seedReview = {
+// 	rating: 2,
+// 	reviewer: 1,
+// 	review: 'This is almost the worst thing ever',
+// 	film: 1
+// }
 
 describe('reviews routes', () => {
 	beforeEach(() => {
@@ -18,14 +19,15 @@ describe('reviews routes', () => {
 	it('creates a new review', () => {
 		const newReview = {
 			rating: 5,
-			reviewer: 1,
 			review: 'This is amazing',
-			film: 2
 		};
 
 		return request(app)
 			.post('/api/v1/reviews')
-			.send(newReview)
+			.send({
+				FilmId: 1,
+				ReviewerId: 1,
+				...newReview})
 			.then(res => expect(res.body).toEqual({
 				id: 1,
 				...newReview
