186d1ebdbe7751ac4aa488046591d8e71593a4f9
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-09 17:57:19 -0700

    Seed a review into the db before each review test, make sure post test still passing

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -4,16 +4,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Review = require('../lib/models/Review');
 
-// const seedReview = {
-// 	rating: 2,
-// 	reviewer: 1,
-// 	review: 'This is almost the worst thing ever',
-// 	film: 1
-// }
+const seedReview = {
+	rating: 2,
+	ReviewerId: 1,
+	review: 'This is almost the worst thing ever',
+	FilmId: 2
+}
 
 describe('reviews routes', () => {
 	beforeEach(() => {
 		return db.sync({ force: true });
+	});
+
+	beforeEach(async () => {
+		await request(app)
+			.post('/api/v1/reviews')
+			.send(seedReview);
 	});
 
 	it('creates a new review', () => {
@@ -28,10 +34,13 @@ describe('reviews routes', () => {
 			.post('/api/v1/reviews')
 			.send(newReview)
 			.then(res => expect(res.body).toEqual({
-				id: 1,
+				id: 2,
 				...newReview
 			}));
 	});
 
 
+	it('gets all reviews', () => {
+
+	})
 });
