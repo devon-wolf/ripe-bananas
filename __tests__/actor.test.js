bdc8741d30212c1249480fe67b86900891921719
SamuelHGerber <henrylightfoot@gmail.com>
2021-04-12 13:51:47 -0700

    tests passing for Actor model

--- __tests__/actor.test.js
+++ __tests__/actor.test.js
@@ -1,84 +1,88 @@
 require('../lib/models/associations');
-const db = require('../lib/utils/database')
+const db = require('../lib/utils/database');
 const request = require('supertest');
-const app = require('../lib/app')
-const Actor = require('../lib/models/Actor')
+const app = require('../lib/app');
+const Actor = require('../lib/models/Actor');
 const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
 
-const actor = { 
-    name: 'Robert Downey Jr',
-    dob: '1965-04-04',
-    pob: 'Manhattan, New York, NY',
-    FilmId: '1'
+const actor = {
+  name: 'Robert Downey Jr',
+  dob: '1965-04-04',
+  pob: 'Manhattan, New York, NY',
 };
-const film = [{ 
-    id: '1',
-    title: 'Iron Man',
-    release: '2008-05-02',
-    ActorId: '1'
 
-}]
+const studio = {
+  name: 'Star Studios',
+  city: 'Portland',
+  state: 'Oregon',
+  country: 'United States',
+};
 
-describe('ripe-banana actor routes', () => { 
-    beforeEach(() => { 
-        return db.sync({ force: true });
-    });
-    beforeEach(async () => { 
-        originalActor = await Actor.create(actor)
-    })
-    beforeEach(async () => { 
-      originalFilm = await Film.create(film)
-    })
+const film = {
+  title: 'Iron Man',
+  released: '2008-05-02',
+  StudioId: 1,
+  ActorId: 1,
+};
 
-    it('creates a new Actor', () => { 
-        const newActor = { 
-            name: 'Robert Downey Jr',
-            dob: '1965-04-04',
-            pob: 'Manhattan, New York, NY'
-        };
+describe('ripe-banana actor routes', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+  beforeEach(async () => {
+    originalActor = await Actor.create(actor);
+  });
+  beforeEach(async () => {
+    originalStudio = await Studio.create(studio);
+  });
+  beforeEach(async () => {
+    originalFilm = await Film.create(film);
+  });
 
-        return request(app)
-            .post('/api/v1/actors')
-            .send(newActor)
-            .then((res) => { 
-                expect(res.body).toEqual({
-                    ...newActor,
-                    id: 2
-                })
-            })
-    })
-    it('gets all Actors', async () => { 
-        return request(app)
-            .get('/api/v1/actors')
-            .then((res) => {
-                expect(res.body).toEqual([{
-                    ...actor,
-                    id: 1,
-                }])
-            })
-    })
- 
-    it('gets an actor by id', async () => { 
-        return request(app)
-            .get('/api/v1/actors/1')
-            .then((res) => { 
-                expect(res.body).toEqual({
-                    ...actor,
-                    id: 1
-                })
-            })
-    })
-    
-    it('gets an actor by id with Film Join', async () => { 
-        return request(app)
-            .get('/api/v1/actors/1')
-            .then((res) => { 
-                expect(res.body).toEqual({
-                    ...actor,
-                    id: 1,
-                    films : [film]
-                })
-            })
-    })
+  it('creates a new Actor', () => {
+    const newActor = {
+      name: 'Robert Downey Jr',
+      dob: '1965-04-04',
+      pob: 'Manhattan, New York, NY',
+    };
 
+    return request(app)
+      .post('/api/v1/actors')
+      .send(newActor)
+      .then((res) => {
+        expect(res.body).toEqual({
+          ...newActor,
+          id: 2,
+        });
+      });
+  });
+  it('gets all Actors', async () => {
+    return request(app)
+      .get('/api/v1/actors')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            ...actor,
+            id: 1,
+          },
+        ]);
+      });
+  });
+
+  it('gets an actor by id with Film Join', async () => {
+    // await request(app).post('/api/v1/films').send(film);
+
+    return request(app)
+      .get('/api/v1/actors/1')
+      .then((res) => {
+        console.log(res.body);
+        console.log(film);
+        expect(res.body).toEqual({
+          ...actor,
+          id: 1,
+          Films: [{ ...film, id: 1 }],
+        });
+      });
+  });
 });
--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -1,4 +1,4 @@
-// require('../models/associations');
+require('../lib/models/associations');
 
 const db = require('../lib/utils/database');
 const request = require('supertest');
