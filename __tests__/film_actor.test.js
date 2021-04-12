74cae246069bbc94a0eb3e6151b823e8dbeb16ac
SamuelHGerber <henrylightfoot@gmail.com>
2021-04-12 09:46:21 -0700

    test written and failing for GET /films_actors

--- __tests__/film_actor.test.js
+++ __tests__/film_actor.test.js
@@ -19,7 +19,7 @@ describe('ripe-bananas film_actor routes', () => {
     originalRole = await Film_Actor.create(role);
   });
 
-  it('creates a new Film and Actor', () => {
+  it('creates a new role', () => {
     const newRole = {
       role: 'Father of Robert',
       actor: 62,
@@ -37,20 +37,20 @@ describe('ripe-bananas film_actor routes', () => {
       });
   });
 
-  //   it('gets all films', () => {
-  //     return request(app)
-  //       .get('/api/v1/films')
-  //       .then((res) => {
-  //         expect(res.body).toEqual([
-  //           {
-  //             id: 1,
-  //             title: 'Forgotten Martians',
-  //             studio: 7,
-  //             released: 1976,
-  //           },
-  //         ]);
-  //       });
-  //   });
+  it('gets all roles', () => {
+    return request(app)
+      .get('/api/v1/films_actors')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            id: 1,
+            role: 'Seventh Astronaut',
+            actor: 38,
+            film: 117,
+          },
+        ]);
+      });
+  });
 
   //   it('gets a single film by id', () => {
   //     return request(app)
